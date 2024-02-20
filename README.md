5e520333e46e424d74a81c55d23cb9a42a37053f
steffanossa <steffanossa@tuta.io>
2024-02-20 01:08:35 +0100

    fix1_2

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # studydrive_downloader
-enables downloads for non-premium users
 
+enables downloads for non-premium users
+wait for the download button to turn magenta
 
 > Friede dem Wellblech, Krieg den Palästen
--- studydrive_downloader.js
+++ studydrive_downloader.js
@@ -3,26 +3,43 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.studydrive.net/*/doc/*
 // @grant       none
-// @version     1.1
+// @version     1.2
 // @author      steffanossa
 // @license     MIT
 // @description 06/09/2023, 13:56:21
 // ==/UserScript==
 
-(function() {
-  'use strict';
-  console.log('Ich grüße meine liebe Oma, die langsamste Frau der Welt ❤️');
+(function () {
+	fetch(document.location.href)
+		.then(res => res.text())
+		.then(res => {
+			var fileName = /display_file_name":"(.*?)"/.exec(res)[1];
+			var url = /file_preview":"(.*?)"/.exec(res)[1];
+			var url = url.replace('\\', '');
 
-  function extractAndOpenLink() {
-    window.open(sdWindow.document.file_preview);
-  }
+			fetch(url)
+				.then(res => res.arrayBuffer())
+				.then(res => {
+					var file = new Blob([res], { type: 'application/pdf' });
+					var fileURL = URL.createObjectURL(file);
 
-  const downloadButton = document.querySelector('[data-specific-auth-trigger="download"]');
-  let buttonCopy;
-  if (downloadButton) {
-    buttonCopy = downloadButton.cloneNode(true);
-    downloadButton.parentNode.replaceChild(buttonCopy, downloadButton);
-    buttonCopy.addEventListener('click', extractAndOpenLink);
-    buttonCopy.title = 'Download';
-  }
+					console.log('Ich grüße meine liebe Oma, die langsamste Frau der Welt ❤️');
+					console.log('Friede dem Wellblech, Krieg den Palästen');
+
+					const downloadButton = document.querySelector('[data-specific-auth-trigger="download"]');
+					let buttonCopy;
+					if (downloadButton) {
+						buttonCopy = downloadButton.cloneNode(true);
+						downloadButton.parentNode.replaceChild(buttonCopy, downloadButton);
+						buttonCopy.addEventListener('click', () => extractAndOpenLink(fileURL));
+						buttonCopy.title = 'Download';
+						buttonCopy.style.color = 'white';
+						buttonCopy.style.background = 'magenta';
+					}
+				});
+		});
+
+	function extractAndOpenLink(fileUrl) {
+		window.open(fileUrl);
+	}
 })();
